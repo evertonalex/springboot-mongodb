0b562fb2a176b54068a77003097b8056df097470
Everton Alex <evertonalexweb@gmail.com>
2018-12-27 15:35:44 -0200

    consulta simples @QUery

--- src/main/java/br/com/evertonalex/services/PostService.java
+++ src/main/java/br/com/evertonalex/services/PostService.java
@@ -29,7 +29,8 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text){
-        return repo.findByTitleContainingIgnoreCase(text);
+//        return repo.findByTitleContainingIgnoreCase(text);
+        return repo.searchTitle(text);
     }
 
 }
