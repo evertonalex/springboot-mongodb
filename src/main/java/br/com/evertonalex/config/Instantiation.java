b8cbe02c273cd3c7e5a9b31540aa7ff9eda01384
Everton Alex <evertonalexweb@gmail.com>
2018-12-27 14:45:47 -0200

    acrescentando comentarios aos posts

--- src/main/java/br/com/evertonalex/config/Instantiation.java
+++ src/main/java/br/com/evertonalex/config/Instantiation.java
@@ -3,12 +3,14 @@ package br.com.evertonalex.config;
 import br.com.evertonalex.domain.Post;
 import br.com.evertonalex.domain.User;
 import br.com.evertonalex.dto.AuthorDTO;
+import br.com.evertonalex.dto.CommentDTO;
 import br.com.evertonalex.repository.PostRepository;
 import br.com.evertonalex.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -39,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "Vou viajar para Sao Paulo, abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem Mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
