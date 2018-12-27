423a9f1e0499e3f99e7e2c8063bba0449b75d066
Everton Alex <evertonalexweb@gmail.com>
2018-12-27 14:58:37 -0200

    consulta simples com query methods

--- src/main/java/br/com/evertonalex/resources/PostResource.java
+++ src/main/java/br/com/evertonalex/resources/PostResource.java
@@ -3,8 +3,10 @@ package br.com.evertonalex.resources;
 import br.com.evertonalex.domain.Post;
 import br.com.evertonalex.domain.User;
 import br.com.evertonalex.dto.UserDTO;
+import br.com.evertonalex.resources.util.URL;
 import br.com.evertonalex.services.PostService;
 import br.com.evertonalex.services.UserService;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,5 +31,13 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
+    }
 
 }
